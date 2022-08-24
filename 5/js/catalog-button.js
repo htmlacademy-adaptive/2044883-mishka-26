8472484ff231bb08086c847d2cf93267461904f9
Keks <keks@htmlacademy.ru>
2022-08-24 17:23:39 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/catalog-button.js
@@ -0,0 +1,11 @@
+let mainCatalogButton = document.querySelector('.catalog-nav__toggle');
+let menu = document.querySelector('.catalog-nav__wrapper');
+
+mainCatalogButton.classList.remove('menu-button--hide');
+menu.classList.remove('catalog-nav__wrapper--nojs');
+menu.classList.add('catalog-nav__wrapper--close');
+
+mainCatalogButton.onclick = function() {
+  mainCatalogButton.classList.toggle('menu-button--close');
+  menu.classList.toggle('catalog-nav__wrapper--close');
+}
