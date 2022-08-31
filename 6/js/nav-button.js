41ad85ee242ff3db6461a5331a5258d9226ea193
Keks <keks@htmlacademy.ru>
2022-08-31 14:01:35 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/nav-button.js
@@ -0,0 +1,11 @@
+const mainNavButton = document.querySelector('.main-nav__toggle');
+const menu = document.querySelector('.main-nav__wrapper');
+
+mainNavButton.classList.remove('menu-button--hide');
+menu.classList.remove('main-nav__wrapper--nojs');
+menu.classList.add('main-nav__wrapper--close');
+
+mainNavButton.onclick = function() {
+  mainNavButton.classList.toggle('menu-button--close');
+  menu.classList.toggle('main-nav__wrapper--close');
+}
