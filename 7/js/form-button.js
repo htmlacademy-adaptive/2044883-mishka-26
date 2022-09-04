c0b8ff1f3e1869837d7629beb0a20d7652024100
Keks <keks@htmlacademy.ru>
2022-09-05 01:14:53 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form-button.js
@@ -0,0 +1,11 @@
+const mainFormButton = document.querySelector('.form-nav__toggle');
+const menu = document.querySelector('.form-nav__wrapper');
+
+mainFormButton.classList.remove('menu-button--hide');
+menu.classList.remove('form-nav__wrapper--nojs');
+menu.classList.add('form-nav__wrapper--close');
+
+mainFormButton.onclick = function() {
+  mainFormButton.classList.toggle('menu-button--close');
+  menu.classList.toggle('form-nav__wrapper--close');
+}
